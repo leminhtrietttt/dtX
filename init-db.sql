ba672e863296e71bdead60c603ef74b618946284
leminhtrietttt <leminhtriet1412@gmail.com>
2024-06-13 19:07:27 +0700

    init-db.sql

--- init-db.sql
+++ init-db.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS tblbookIUHX (
+CREATE TABLE IF NOT EXISTS tblbook1469957 (
     id serial PRIMARY KEY,
     title varchar NOT NULL,
     author varchar NOT NULL
 );
 
 INSERT INTO tblbook (title, author) VALUES
-  ('DevOps', 'IUHX'),
-  ('Big Data', 'IUHX'),
-  ('Cloud Deployement', 'IUHX'),
-('Data Analysis', 'IUHX'),
-('Block Chain', 'IUHX');
+  ('DevOps', '1469957'),
+  ('Big Data', '1469957'),
+  ('Cloud Deployement', '1469957'),
+('Data Analysis', '1469957'),
+('Block Chain', '1469957');
